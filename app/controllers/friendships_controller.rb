599eea23e43c851fa2bc4dc865cbd5dd63931ec9
petumazo <davidalvarezmazzo@gmail.com>
2021-07-19 13:22:55 -0500

    Create Friendship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,22 @@
 class FriendshipsController < ApplicationController
+    def index ; end
+    
+    
     def create
-        current_user.friendships.create(friend_id: params[:invited_user_id], confirmed: false)
+        user = User.find(params[:invited_user_id])
+        friendship = Friendship.create(user_id: current_user.id, friend_id: params[:invited_user_id], confirmed: false)
+        if friendship.save
+            redirect_to user, notice: 'Your friendship invitation was successfully sent.'
+        end
     end
 
     #Accept friendship
     def update
-        
+        inviter_user = User.find(params[:id])
+        current_user.confirm_friend inviter_user
+        if current_user.friend? inviter_user
+            redirect_to inviter_user, notice: 'Your friendship invitation was accepted.'
+        end
     end
     
 
