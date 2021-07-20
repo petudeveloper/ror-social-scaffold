fc3f25c0c869d2a10d923568df780c14c3a7cbf5
petumazo <davidalvarezmazzo@gmail.com>
2021-07-20 16:26:22 -0500

    Correct linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,19 +13,17 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
-  #find user friends only by user_id in the friendships table
-  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
+  # find user friends only by user_id in the friendships table
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
   has_many :friends, through: :confirmed_friendships
 
   # Users who need to confirm friendship
-  has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
   has_many :pending_friends, through: :pending_friendships, source: :friend
 
   # Users who requested to be friends (needed for notifications)
-  has_many :inverted_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :friend_requests, through: :inverted_friendships
-
-
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
