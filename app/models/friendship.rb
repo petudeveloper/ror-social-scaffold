4e60fef65ae43f4e779e45c39c9d005a8a04ed89
petumazo <davidalvarezmazzo@gmail.com>
2021-07-20 11:24:52 -0500

    Add validations to friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,8 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+  belongs_to :inviter, class_name: 'User'
+
+  validates :user_id, uniqueness: { scope: :friend_id, message: 'You have already sent a request to this user' }
+  validates :inviter_id, presence: true
 end
