f9af963cf34a2a0144befe3e080daf48d16068c2
petumazo <davidalvarezmazzo@gmail.com>
2021-07-20 16:11:22 -0500

    Create an additional low with reversed user-friend pair.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,8 +1,11 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
-  belongs_to :inviter, class_name: 'User'
 
-  validates :user_id, uniqueness: { scope: :friend_id, message: 'You have already sent a request to this user' }
-  validates :inviter_id, presence: true
+  def confirm_friend
+    self.update_attributes(confirmed: true)
+    Friendship.create!(friend_id: self.user_id,
+                    user_id: self.friend_id,
+                    confirmed: true)
+  end  
 end
